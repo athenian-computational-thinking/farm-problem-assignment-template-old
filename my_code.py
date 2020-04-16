aa614d0c8a069e6d1fc1697f883b84f9d3e0e971
Megan Leich <mleich@athenian.org>
2020-04-16 02:50:17 +0000

    Add starter code

--- my_code.py
+++ my_code.py
@@ -1,13 +1,15 @@
-def inc(x):
+def legs(X, y, z):
     # User code goes here
-    return x + 1
+    return # add variable to return here
 
 
 if __name__ == '__main__':
     # Test your code with this first
-    # Change the argument to try different values
-    print(inc(6))
+    # Change the arguments to try different values
+    print(legs(2,2,2))
 
-    # After you are satisfied with your results, use input() to prompt the user for a value:
-    #v = input("Value to increment: ")
-    #print(inc(int(v)))
+    # After you are satisfied with your results, use input() to prompt the user for values:
+    #chickens = int(input("Enter number of chickens: "))
+    #cows = (complete this line)
+    #pigs = (complete this line)
+    #print(legs(chickens,cows,pigs))
